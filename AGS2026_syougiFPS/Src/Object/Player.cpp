0128125087dcfd7647c2d3453912eb0353c31afb
ASOST\2416041 <shoukoutaiyilang385@gmail.com>
2026-05-01 12:52:25 +0900

    player

---
+++ AGS2026_syougiFPS/Src/Object/Player.cpp
@@ -0,0 +1,20 @@
+#include "Player.h"
+
+Player::Player(void)
+{
+}
+Player::~Player(void)
+{
+}
+
+void Player::Init(void)
+{
+}
+
+void Player::Update(void)
+{
+}
+
+void Player::Draw(void)
+{
+}
---
+++ AGS2026_syougiFPS/Src/Object/Player.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<DxLib.h>
+class Player
+{
+private:
+	
+
+
+	void Init(void);
+	void Update(void);
+	void Draw(void);
+
+public:
+	
+};
+
