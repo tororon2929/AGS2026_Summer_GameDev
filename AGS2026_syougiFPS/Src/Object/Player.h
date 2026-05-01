2071ee2582d6318a7de9cc5ececa82159f0bdebb
ASOST\2416041 <shoukoutaiyilang385@gmail.com>
2026-05-01 15:21:22 +0900

    player

--- AGS2026_syougiFPS/Src/Object/Player.h
+++ AGS2026_syougiFPS/Src/Object/Player.h
@@ -3,7 +3,12 @@
 class Player
 {
 private:
-	
+	// スピード
+	static constexpr float SPEED_MOVE = 5.0f;
+	static constexpr float SPEED_RUN = 10.0f;
+
+	// ジャンプ力
+	static constexpr float POW_JUMP = 35.0f;
 
 
 	void Init(void);
